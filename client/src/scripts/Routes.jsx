06debba08a432be93a3bb5add5e5dca530a8aa34
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-06-06 23:19:23 +0530

    baseline

--- client/src/scripts/Routes.jsx
+++ client/src/scripts/Routes.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-import App from './home/App';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import App from './home/App';
+import Error from './Error';
 
 export default class Routes extends Component {
     render() {
         return (
             <Router>
                 <div>
-                    <Route path='/' exact component={App} />
+                    <Switch>
+                        <Route path='/' exact component={App} />
+                        <Route component={()=><Error msg='404'/>}/>
+                    </Switch>
                 </div>
             </Router>
         )
